f0842edacba4be7edcc1db6180e8412a87420923
miki <miki@email.jp>
2024-05-24 16:49:15 +0900

    miki 1 commit

--- src/main/java/com/example/demo/form/AkuchibuController.java
+++ src/main/java/com/example/demo/form/AkuchibuController.java
@@ -1,12 +1,25 @@
 package com.example.demo.form;
 
+
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AkuchibuController {
 	@RequestMapping("/")
-	public String top() {
+	public String chat1() {
 		return "chat1";
 	}
+	
+	
+
+	@RequestMapping("/top")
+	public String top() {
+		return "chat2";
 	}
+	
+	}
+	
+
